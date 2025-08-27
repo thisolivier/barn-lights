fe8da264279a30fe1b93abd2e09d380d5658a42a
Olivier <olivier.butler@gmail.com>
2025-08-26 23:22:20 -0700

    feat: bundle ui with webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import { dirname, resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const currentDirectory = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  context: resolve(currentDirectory, 'src/ui'),
+  entry: './root.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(currentDirectory, 'src/ui/dist'),
+    clean: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(currentDirectory, 'src/ui/index.html')
+    })
+  ]
+};
+
